9854a440c686f496c059e40acc8f236b97e52552
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2022-08-09 12:21:30 +0300

    initiate commit

---
+++ api/v1alpha1/mongodb_types.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// MongoDBSpec defines the desired state of MongoDB
+type MongoDBSpec struct {
+	// Port is the port number exposed in the service; if not set, the service port will be 27017
+	Port int32 `json:"port,omitempty"`
+
+	// StorageClassName for the PVC; if missing, the StorageClass will be the cluster default
+	StorageClassName *string `json:"storageClassName,omitempty"`
+
+	// Replicas is the desired number of nodes in the mongodb replica set
+	// +kubebuilder:default:=3
+	// +kubebuilder:validation:Enum=3;5;7;9
+	Replicas *int32 `json:"replicas,omitempty"`
+}
+
+// MongoDBStatus defines the observed state of MongoDB
+type MongoDBStatus struct {
+	// Conditions tells the current status of the resource
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// ConnectionURL may be used by a client to connetc to the mongoDB replica set
+	ConnectionURL *string `json:"connectionURL,omitempty"`
+
+	// Replicas is the actual number of replicas in the mongoDB replica set
+	Replicas *int32 `json:"replicas,omitempty"`
+}
+
+// +kubebuilder:resource:shortName=mdb
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas
+
+// MongoDB is the Schema for the mongodbs API
+type MongoDB struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MongoDBSpec   `json:"spec,omitempty"`
+	Status MongoDBStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// MongoDBList contains a list of MongoDB
+type MongoDBList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MongoDB `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MongoDB{}, &MongoDBList{})
+}
---
+++ controllers/mongodb_controller_test.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"context"
+
+	api "github.com/nunnatsa/mongodb-operator/api/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var _ = Describe("test the controller", func() {
+	It("should create a deployment and a service", func() {
+		mdb := &api.MongoDB{
+			TypeMeta: metav1.TypeMeta{APIVersion: "mongodb.example.com/v1alpha1", Kind: "MongoDB"},
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "testdb",
+			},
+		}
+		client := fake.NewClientBuilder().WithObjects(mdb).WithScheme(setupScheme()).Build()
+
+		r := MongoDBReconciler{
+			Client:   client,
+			Scheme:   setupScheme(),
+			MDBImage: "image-name",
+		}
+
+		res, err := r.Reconcile(context.Background(), ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "default", Name: "testdb"}})
+		Expect(err).To(HaveOccurred())
+		Expect(res.IsZero()).To(BeTrue())
+	})
+})
+
+func setupScheme() *runtime.Scheme {
+	s := runtime.NewScheme()
+
+	if err := api.AddToScheme(s); err != nil {
+		panic(err)
+	}
+	if err := appsv1.AddToScheme(s); err != nil {
+		panic(err)
+	}
+	if err := corev1.AddToScheme(s); err != nil {
+		panic(err)
+	}
+
+	return s
+}
